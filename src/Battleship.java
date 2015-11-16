21f68131b4c67af43810069b752a9bdbe695a5e7
torgammelgard <tor.gammelgard@gmail.com>
2015-11-16 01:44:42 +0100

    Added a comment to test commit and push

--- src/Battleship.java
+++ src/Battleship.java
@@ -14,6 +14,7 @@ import javax.swing.*;
  * The ships are randomly oriented and placed.
  * The ships have at least one square in between each other.
  *
+ * Project was written in IntelliJ IDEA 15
  * @author Tor Gammelgard
  * @version 2015-10-15
  */
